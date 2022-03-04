66a1e034a2e45e6c62b5d09e46e674667b75d766
Mateus Duarte <mateus_duarte20@outlook.com>
2022-03-04 15:03:55 -0300

    Adicionei o componente Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header/index'
 import { GlobalStyle } from './global';
+import { Main } from './components/Main'
 
 
 function App() {
   return (
     <>
       <Header />
+       <Main />
       <GlobalStyle />
       </>
   );
