f46287598c1d71bf626595c2cd22ed54b4b78793
Mateus Duarte <mateus_duarte20@outlook.com>
2022-03-05 06:18:05 -0300

    Criei uma nova variavel chamada copylevel

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -18,7 +18,9 @@ export const calculateImc = (height: number, weight: number) => {
 
     for(let i in levels){
         if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]) {
-            levels[i].yourImc = parseFloat(imc.toFixed(2));
+            let copyLevel: Level = { ...levels[i]}
+            copyLevel.yourImc = parseFloat(imc.toFixed(2));
+            
             return levels[i];
         }
     }
