c1cb6ab5aa7446481f0c55d364c9c3bac748629e
Mateus Duarte <mateus_duarte20@outlook.com>
2022-03-05 04:57:47 -0300

    Criei a pasta pages

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import {Header} from '../components/Header'
+import {Main} from '../components/Main'
+
+export const Dashboard = () => {
+    return (
+        <>
+        <Header />
+        <Main />
+        </>
+    )
+}
